16a31d1d3eafacff801e5092a4d28ecd340b1d5b
millan <millanpoudel1@gmail.com>
2022-06-10 08:10:19 +0530

    shop page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
 import Welcome from './components/Welcome';
-
+import Shop from "./components/Shop"
 function App() {
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
         <Navigation/>
         <Routes>
           <Route exact path="/" element={<Welcome/>}></Route>
+          <Route exact path="/shop" element={<Shop/>}></Route>
         </Routes>
       </Router>
           </div>
