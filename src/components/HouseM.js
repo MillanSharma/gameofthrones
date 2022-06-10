e0acac1e7165b9729106dbe3372c60aa4ba33b78
millan <millanpoudel1@gmail.com>
2022-06-10 15:53:18 +0530

    houses page mobile

---
+++ src/components/HouseM.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import "./HouseM.css"
+import useCollapse from 'react-collapsed';
+function HouseM() {
+    const config = {
+        duration:1500
+    }
+const [isExpanded, setExpanded] = useState(false);
+const [items,setItems] =useState([]);
+const { getCollapseProps, getToggleProps } = useCollapse(config);
+useEffect(() => {
+        axios
+          .get("https://www.anapioficeandfire.com/api/houses")
+          .then((response) => setItems(response.data))
+          .catch((err) => console.log(err));
+      }, []);
+  return (
+    <div className='houseM__container'>
+       <div className='houseM__innerContainer'>
+            {items.map((item,key=item.name)=>(
+              <div  key={key} className="houseM__content">
+                <div className='collapsible'>
+               <div className='header' {...getToggleProps(
+                {
+                    onClick:()=>setExpanded((prevExpanded)=>!prevExpanded)
+
+                }
+               )}
+               >
+                {item.name}</div>
+               <div {...getCollapseProps()}>
+                <div className='content'>
+                       <p>Coat of Arms:<br/>{item.coatOfArms}</p> 
+               <p>Region: <br/>{item?.region}</p>
+               <p>Words:<br/>{item?.words}</p>
+               </div>
+               </div>
+              </div>
+            </div>
+                ))}
+          
+
+        </div>
+
+    </div>
+  )
+}
+
+export default HouseM
--- src/components/Houses.js
+++ src/components/Houses.js
@@ -1,9 +1,20 @@
 import React, { useState ,useEffect} from 'react'
 import "./Houses.css";
 import axios from "axios"
+import HouseM from './HouseM.js';
 function About() {
     const [hover, setHover] = useState(false);
     const [items,setItems] =useState([]);
+    const [windowWidth, setWindowWidth] = useState(0);
+    let resizeWindow = () => {
+      setWindowWidth(window.innerWidth);
+    };
+  
+    useEffect(() => {
+      resizeWindow();
+      window.addEventListener("resize", resizeWindow);
+      return () => window.removeEventListener("resize", resizeWindow);
+    }, []);
     useEffect(() => {
         axios
           .get("https://www.anapioficeandfire.com/api/houses")
@@ -11,18 +22,22 @@ function About() {
           .catch((err) => console.log(err));
       }, []);
   return (
+    <>
+    {windowWidth <= 710 ? (
+   <HouseM />
+    ):<>
     <div className='page__container'>
       <div className='about__container'>
         
         <div className='scrollable__container'>
             {items.map((item,key=item.name)=>(
-                <div  key={key}
-                 className={`scrolls__${item.name.substring(item.name.lastIndexOf(" ")+1)}`}>
+              <div  key={key}
+              className={`scrolls__${item.name.substring(item.name.lastIndexOf(" ")+1)}`}>
                <div className={`scrolls__img${item.name.substring(item.name.lastIndexOf(" ")+1)}`}/>
                <div className='scroll__title'><h2>{item.name}</h2></div>
                <div onMouseEnter={()=>setHover(true)}
                     onMouseLeave={()=>setHover(false)}
-                className='scroll__content'>
+                    className='scroll__content'>
                        <p>Coat of Arms:<br/>{item.coatOfArms}</p> 
                <p>Region: <br/>{item?.region}</p>
            <p>Titles:<br/>{item?.titles}</p>
@@ -37,6 +52,8 @@ function About() {
 
     </div>
     </div>
+    </>}
+                </>
   )
 }
 
